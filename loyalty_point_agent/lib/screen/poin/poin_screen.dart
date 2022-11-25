d2b455b44f4047b878ada5fdc9b20647856ec2a1
HamdanZulfan <hamdanzulfan28@gmail.com>
2022-11-25 21:13:37 +0700

    fix

--- loyalty_point_agent/lib/screen/poin/poin_screen.dart
+++ loyalty_point_agent/lib/screen/poin/poin_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:loyalty_point_agent/utils/theme.dart';
 
 class PoinScreen extends StatelessWidget {
   const PoinScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: grayishColor,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hai, Kartika Noviyanti!',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.star,
+                  color: yellowColor,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '1000',
+                  style: yellowTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        toolbarHeight: 70,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_none,
+              size: 30,
+              color: navyColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              size: 30,
+              color: navyColor,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- loyalty_point_agent/lib/screen/pulsa/pulsa_screen.dart
+++ loyalty_point_agent/lib/screen/pulsa/pulsa_screen.dart
@@ -5,6 +5,8 @@ class PulsaScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Text('test pulsa')),
+    );
   }
 }
