4e172a78afad9088da74bbcf83c0b37ccd8ad1fe
Ahmad Krisdiantoro <achmadcrizdiantho10@gmail.com>
2022-12-07 03:02:23 +0700

    get Paket Data

---
+++ loyalty_point_agent/lib/providers/paket_data_provider.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:loyalty_point_agent/models/paket_data_model.dart';
+import 'package:loyalty_point_agent/services/paket_data_service.dart';
+import 'package:loyalty_point_agent/utils/finite_state.dart';
+
+class PaketDataProvider extends ChangeNotifier {
+  final PaketDataService service = PaketDataService();
+
+  PaketDataModel? data;
+  MyState myState = MyState.loading;
+
+  Future fetchPaketData() async {
+    myState = MyState.loading;
+    notifyListeners();
+    try {
+      data = await service.getPaketData();
+
+      myState = MyState.loaded;
+      notifyListeners();
+    } catch (e) {
+      if (e is DioError) {
+        e.response!.statusCode;
+      }
+      myState = MyState.failed;
+      notifyListeners();
+    }
+  }
+}
