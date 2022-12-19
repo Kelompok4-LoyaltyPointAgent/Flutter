0fce15f797809d91b233558ad4e067f7b339badf
Ahmad Krisdiantoro <achmadcrizdiantho10@gmail.com>
2022-12-19 18:39:25 +0700

    ganti base url api

--- loyalty_point_agent/lib/utils/urls.dart
+++ loyalty_point_agent/lib/utils/urls.dart
@@ -1,6 +1,6 @@
 class Urls {
   static const String baseUrl =
-      'https://loyaltypointagent-staging-7vx5k3vnra-uc.a.run.app/';
+      'https://loyaltypointagent-7vx5k3vnra-uc.a.run.app/';
 
   static const String login = 'api/v1/login';
   static const String register = 'api/v1/register';
